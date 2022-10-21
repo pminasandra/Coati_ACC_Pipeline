a938a7e621bd67d1861b7c7faa810330330080a0
Pranav Minasandra <pminasandra@ab.mpg.de>
2022-10-21 18:36:16 +0200

    Sanity checks updated

--- sanity_checks.py
+++ sanity_checks.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 import accreading
 import auditreading
+import calibration
 import config
 import utilities
 
@@ -18,10 +19,15 @@ print(f"{os.path.basename(__file__)}: initiated")
 all_audits = auditreading.read_all_audits()
 acc_file = f'{config.DATA_DIR}acc/tag9478_acc.txt'
 df = accreading.read_acc_file(acc_file)
+assert calibration.calibration_file_exists(acc_file)
+df = calibration.calibrate_data(df, calibration.calibration_file(acc_file))
 
 audit_count = 1
 for df_audit in all_audits:
     print(f"{os.path.basename(__file__)}: sanity check, plotting audit {audit_count}")
+    st = list(df_audit['state'])
+    st = [config.REDUCED_STATE[state] for state in st]
+    df_audit['state'] = pd.Series(st)
     df_audit.sort_values(by='datetime', inplace=True, ignore_index=True)
     start_time = df_audit['datetime'].min() - dt.timedelta(seconds=20)
     stop_time = df_audit['datetime'].max() + dt.timedelta(seconds=20)
@@ -31,7 +37,7 @@ for df_audit in all_audits:
 
     df_audits = df_audit.groupby('state')
     for state, vals in df_audits: 
-        axs[0].scatter(x=vals['datetime'], y=[3000]*len(vals['datetime']), label=state, s=0.9)
+        axs[0].scatter(x=vals['datetime'], y=[2]*len(vals['datetime']), label=state, s=0.9)
 
     axs[0].legend()
     utilities.saveimg(fig, f"audit{audit_count}")
--- utilities.py
+++ utilities.py
@@ -32,7 +32,7 @@ def acc_visualise(acc_reads, start_time, stop_time):
 
     acc_relevant = acc_reads[(acc_reads['datetime'] > start_time) & (acc_reads['datetime'] < stop_time)]
 
-    fig, axs = plt.subplots(3,1, sharex=True, sharey=False)
+    fig, axs = plt.subplots(3,1, sharex=True, sharey=True)
     axs[0].plot(acc_relevant['datetime'], acc_relevant['x'], linewidth=0.5)
     axs[1].plot(acc_relevant['datetime'], acc_relevant['y'], linewidth=0.5)
     axs[2].plot(acc_relevant['datetime'], acc_relevant['z'], linewidth=0.5)
